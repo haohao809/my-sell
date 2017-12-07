c940c13bff9ec832f7a340fb144aa4784dab9655
haohao809 <haohao809@126.com>
2017-12-07 16:39:21 +0800

    food

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -8,7 +8,18 @@
 				</div>
 			</div>
 			<div class="content">
-				
+				<h1 class='title'>{{food.name}}</h1>
+				<div class="detail">
+					<span class='sell-count'>月售份{{food.sellCount}}份</span>
+					<span calss='rating'>好评率{{food.rating}}%</span>
+				</div>
+				<div class="price">
+				 <span class="now">￥{{food.price}}</span>
+				 <span class="old">￥{{food.oldPrice}}</span>
+				</div>
+				<div class="cartcontrol-wrapper">
+					
+				</div>
 			</div>
 			<div class="split">
 				
@@ -62,7 +73,7 @@
 		bottom: 48px;
 		left: 0;
 		right: 0;
-		z-index: 80;
+		z-index: 30;
 		background: #fff;
 		.image-header{
 			position: relative;
@@ -88,6 +99,43 @@
 				}
 			}
 
+		}
+		.content{
+			position: relative;
+			padding: 18px;
+			.title{
+				font-size: 14px;
+				line-height: 14px;
+				font-weight: bold;
+				color: #07111b;
+				margin-bottom: 8px;
+			}
+			.detail{
+				font-size: 10px;
+				color:#93999f;
+				margin-bottom: 10px;
+			}
+			.price{
+				line-height: 14px;
+				font-weight: bold;
+				font-size: 14px;
+				.now{
+					color: #f01414;
+					font-size: 14px;
+				}
+				.old{
+					color: #93999f;
+					font-size: 10px;
+					text-decoration: line-through;
+				}
+			}
+		}
+		.split{
+			width: 100%;
+			height: 16px;
+			border-bottom: 1px solid rgba(7,17,27,0.1);
+			border-top: 1px solid rgba(7,17,27,0.1);
+			background: #f3f5f7;
 		}
 	}
 </style>

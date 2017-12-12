e43a9789225cb411d46f35093904be70dd7d82a1
haohao809 <haohao809@126.com>
2017-12-12 12:13:34 +0800

    ratings

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="ratings">
+	<scroll class="ratings" :data='ratings'>
 		<div class="ratings-content">
 			<div class="overview">
 				<div class="over-left">
@@ -19,7 +19,7 @@
 					</div>
 					<div class="score-wrapper">
 						<span class='text'>商品评分</span>
-						<star :score='seller.foodScore'></star>
+						<star :score='seller.foodScore' size="36"></star>
 						<span class="score">{{seller.foodScore}}</span>
 					</div>
 					<div class="delivery">
@@ -37,18 +37,41 @@
 			
 		</div>
 		<div class="rating-wrapper">
-			
+			<ul>
+				<li v-for='(rate,index) in ratings' class="rate-item">
+					<div class="avatar">
+						<img :src="rate.avatar" width="28" height="28" />
+					</div>
+					<div class="content">
+						<h1 class="name">{{rate.username}}</h1>
+						<span class='time'>{{rate.rateTime|format}}</span>
+						<div class="star-wrapper">
+							<star :score='rate.score' size="24"></star>
+							<span class="delivery" v-show='rate.deliveryTime'>{{rate.deliveryTime}}分钟</span>
+						</div>
+						<p class="text">{{rate.text}}</p>
+						<div class="recommend" v-show="rate.recommend && rate.recommend.length">
+							<i class='icon-thumb_up'></i>
+							<span class="item" v-for='item in rate.recommend'>{{item}}</span>
+						</div>
+					</div>
+
+					
+				</li>
+			</ul>
 		</div>
-	</div>
+	</scroll>
 </template>
 
 <script>
 	import ratingTab from '@/components/ratingtab/ratingtab'
 	import star from '@/components/star/star'
+	import Scroll from '@/components/scroll/scroll'
 	export default{
 		components:{
 			ratingTab,
-			star
+			star,
+			Scroll
 		},
 		props:{
 			seller: Object
@@ -59,7 +82,15 @@
 					all: 0,
 					recommend: 0,
 					whine: 0
-				}
+				},
+				ratings: [],
+			}
+		},
+		filters:{
+			format(value){
+				if(!value) return;
+				let formatTime = new Date(value);
+				return formatTime.toLocaleString();
 			}
 		},
 		created(){
@@ -70,6 +101,7 @@
 				console.log(response);
 				if(response.errno === 0){
 					let ratingsArr = response.data;
+					this.ratings = ratingsArr;
 					this.count.all = ratingsArr.length;
 					let goodrating = 0;
 					let badrating = 0
@@ -143,7 +175,61 @@
 	.tab-wrapper{
 		margin: 0 18px;
 	}
-	
+	.rating-wrapper{
+		margin: 0 18px;
+		.rate-item{
+			display: flex;
+        	padding: 18px 0;
+        	border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+        	position: relative;
+			.avatar img{
+				border-radius: 50%;
+			}
+			.content{
+				position: relative;
+				flex: 1;
+				margin-left: 12px;
+				.name{
+					font-size: 10px;
+				}
+				.time{
+					font-size: 10px;
+					position: absolute;
+					right: 18px;
+					color: #93999f;
+					top: 0;
+				}
+				.star-wrapper{
+					.delivery{
+						font-size: 10px;
+						color: #93999f;
+					}
+				}
+				.text{
+					font-size: 10px;
+					margin-top: 5px;
+					line-height: 18px;
+				}
+				.recommend{
+					.icon-thumb_up{
+						color: #00a0dc;
+						font-size: 9px;
+					}
+					.item{
+						font-size: 10px;
+						border: 1px solid rgba(7,17,27,0.1);
+						display: inline-block;
+						margin: 3px;
+						padding: 3px 5px;
+						color: #93999f;
+						background: #fff;
+					}
+				}
+				
+			}
+		}
+
+	}
 	
 }
 
